5407e5bbfe3673b7a177d746acc6a0677906cb5c
gcusaaa <gcusaaa@gmail.com>
2023-12-19 08:43:41 +0200

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form @submit.prevent="sendInfo">
+      <div>
+        <label>Enter a value: </label>
+        <input type="text" placeholder="Enter your value" v-model="currValue" @keyup="popUpMessage(currValue)">
+      </div>
+      <div>
+        <h2>List of entered values</h2>
+        <ul v-show="listOfvalues.length">
+          <li v-for="data, index in listOfvalues" :key="index">
+            <p>{{ data }} @ position {{ index }}</p>
+          </li>
+        </ul>
+      </div>
+      <button type="submit">Display</button>
+    </form>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      currValue: '',
+      listOfvalues: []
+    }
+  },
+  components: {},
+  methods: {
+    sendInfo() {
+      if (this.currValue)
+        this.listOfvalues.push(this.currValue)
+    },
+    popUpMessage(arg) {
+      alert(arg)
+    }
   }
 }
 </script>
